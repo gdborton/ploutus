4feba8f1083cdc6ae930fbf9a806a9154901171b
OpenShift guest <gdborton@gmail.com>
2013-07-30 19:50:56 -0400

    Fixes form view model layout.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,3 @@
-require(['../lib/knockout-2.3.0', 'viewModels/appViewModel'], function(ko, appViewModel) {
-    ko.applyBindings(new appViewModel());
+require(['../lib/knockout-2.3.0', 'viewModels/appViewModel', '../lib/jquery-1.10.2.min'], function(ko, appViewModel) {
+    
 });
--- javascripts/viewModels/appViewModel.js
+++ javascripts/viewModels/appViewModel.js
@@ -1,11 +1,25 @@
 // Main viewmodel class
 define(['../../lib/knockout-2.3.0'], function(ko) {
-    return function appViewModel() {
+    function appViewModel() {
         var self = this;
         
         self.grossIncome = ko.observable(0);
-        self.four01k = ko.observable(0);
+        self._401k = ko.observable(0);
         self.roth = ko.observable(0);
         self.afterTax = ko.observable(0);
+        
+        self.taxRate = ko.computed(function() {
+            return 0.15;
+        });
+        
+        self.netIncome = ko.computed(function() {
+            return ( self.grossIncome() - self._401k() ) * ( 1 - self.taxRate() );
+        });
+        
+        self.yearlySpend = ko.computed(function() {
+            return self.netIncome() - self.roth() - self.afterTax();
+        });
     };
+    
+    ko.applyBindings(new appViewModel())
 });
