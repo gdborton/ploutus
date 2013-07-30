fa6674fd6aa14abf0c49c6dddb994c3ce70c3ed3
OpenShift guest <gdborton@gmail.com>
2013-07-30 19:17:22 -0400

    Changes name attr on inputs to id, starts to add data-binding.

--- javascripts/viewModels/appViewModel.js
+++ javascripts/viewModels/appViewModel.js
@@ -2,5 +2,10 @@
 define(['../../lib/knockout-2.3.0'], function(ko) {
     return function appViewModel() {
         var self = this;
+        
+        self.grossIncome = ko.observable(0);
+        self.four01k = ko.observable(0);
+        self.roth = ko.observable(0);
+        self.afterTax = ko.observable(0);
     };
 });
