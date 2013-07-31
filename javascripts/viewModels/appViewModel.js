7e9062648df7d24fd78d2c92aa91935e8a51e425
OpenShift guest <gdborton@gmail.com>
2013-07-30 20:51:09 -0400

    Adds yearly, monthly, weekly, daily spend calculations.

--- javascripts/viewModels/appViewModel.js
+++ javascripts/viewModels/appViewModel.js
@@ -13,13 +13,33 @@ define(['../../lib/knockout-2.3.0'], function(ko) {
         });
         
         self.netIncome = ko.computed(function() {
-            return ( self.grossIncome() - self._401k() ) * ( 1 - self.taxRate() );
+            return Round( self.grossIncome() - self._401k() ) * ( 1 - self.taxRate() );
         });
         
         self.yearlySpend = ko.computed(function() {
-            return self.netIncome() - self.roth() - self.afterTax();
+            return Round(self.netIncome() - self.roth() - self.afterTax());
         });
-    };
+        
+        self.monthlySpend = ko.computed(function() {
+            return Round(self.yearlySpend() / 12);
+        });
+        
+        self.weeklySpend = ko.computed(function() {
+            return Round(self.monthlySpend() / 4);
+        });
+        
+        self.dailySpend = ko.computed(function() {
+            return Round(self.monthlySpend() / 30.4);
+        });
+        
+        self.yearlyInvestment = ko.computed(function() {
+            return Round(+self._401k() + +self.roth() + +self.afterTax());
+        });
+        
+        function Round(number) {
+            return Math.round(number * 100) / 100;
+        }
+    }
     
-    ko.applyBindings(new appViewModel())
+    ko.applyBindings(new appViewModel());
 });
