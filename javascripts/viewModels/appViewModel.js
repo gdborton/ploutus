7ce0118cc75fc1ec86cd3aea76ccc87d5da80f8b
OpenShift guest <gdborton@gmail.com>
2013-07-30 22:33:13 -0400

    Adds logic for portfolio values after an amount of time.

--- javascripts/viewModels/appViewModel.js
+++ javascripts/viewModels/appViewModel.js
@@ -7,6 +7,8 @@ define(['../../lib/knockout-2.3.0'], function(ko) {
         self._401k = ko.observable(0);
         self.roth = ko.observable(0);
         self.afterTax = ko.observable(0);
+        self.principal = ko.observable(0);
+        self.returnRate = ko.observable(0.07);
         
         self.taxRate = ko.computed(function() {
             return 0.15;
@@ -36,6 +38,28 @@ define(['../../lib/knockout-2.3.0'], function(ko) {
             return Round(+self._401k() + +self.roth() + +self.afterTax());
         });
         
+        // Returns the value of the retirement accounts for years 0-100.
+        self.retirementAccountSeries = ko.computed(function() {
+            var series = [];
+            self.yearlyInvestment();
+            for(var i = 0; i<100; i++) {
+                series.push(valueAfterYears(i))
+            };
+            return series.toString();
+        });
+        
+        //  Returns the value of the retirement accounts after a specified number of years.
+        function valueAfterYears(years) {
+            var p = +self.principal();
+            var r = +self.returnRate();
+            var z = 1+r;
+            var c = +self.yearlyInvestment();
+            
+            return (p * Math.pow(z, years)) + c * ( (Math.pow(z, (years+1)) - z) / r);
+            
+            // Math for this method found at http://www.moneychimp.com/articles/finworks/fmbasinv.htm
+        }
+        
         function Round(number) {
             return Math.round(number * 100) / 100;
         }
@@ -43,3 +67,25 @@ define(['../../lib/knockout-2.3.0'], function(ko) {
     
     ko.applyBindings(new appViewModel());
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
