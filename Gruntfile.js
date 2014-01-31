806fe78345e3b5c9cc5b26d0336eb0db382ab1e0
Gary <gborton@exacttarget.com>
2014-01-31 12:49:49 -0500

    Configure the initial requirejs compiler.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,8 +5,17 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-requirejs');
 
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+		requirejs: {
+			compile: {
+				options: {
+					appDir: 'src/',
+					mainConfigFile: 'src/fire-when-ready/javascripts/main.js',
+					dir: 'dist/'
+				}
+			}
+		}
     });
 
-    grunt.registerTask('default', []);
+    grunt.registerTask('default', ['requirejs:compile']);
 };
