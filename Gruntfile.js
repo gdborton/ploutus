697428a1f2e8459322398b5b1f727e86cd8a253a
Gary <gborton@exacttarget.com>
2014-01-31 12:41:10 -0500

    Adds grunt-contrib-requirejs as a dependency.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,9 @@
 module.exports = function (grunt) {
     'use strict';
 
+	//https://github.com/gruntjs/grunt-contrib-requirejs
+	grunt.loadNpmTasks('grunt-contrib-requirejs');
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json')
     });
