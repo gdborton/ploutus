38e68f163186f21d42a61e2174c817e29ab1494e
Gary Borton <gborton@exacttarget.com>
2014-03-31 22:59:28 -0400

    Move Jekyll files out of the src directory.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,9 @@ module.exports = function (grunt) {
     'use strict';
 
     grunt.loadNpmTasks('grunt-contrib-requirejs');
-
     grunt.loadNpmTasks('grunt-contrib-clean');
-
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
